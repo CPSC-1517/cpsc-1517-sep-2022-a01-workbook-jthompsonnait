c44d52f120c0bbfb81c128ec20c9264dcea0238f
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2022-09-15 16:49:01 -0600

    Person

---
+++ src/CSharpGrammar/PracticeConsole/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole.Data
+{
+    public  class Person
+    {
+        //  Example of a composite class
+        //  Each instance of this class will represent an individual
+
+        //  This class define the fillowing charactersitics of a person
+        //  First Name, Last Name, Current resident address
+        //   List of employment positions.
+
+        private string _FirstName;
+        private string _LastName;
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                if (Utilities.IsEmpty(value))
+                {
+                    throw new ArgumentNullException("First name is required");
+                }
+                _FirstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                if (Utilities.IsEmpty(value))
+                {
+                    throw new ArgumentNullException("Last name is required");
+                }
+                _LastName = value;
+            }
+        }
+
+        public ResidentAddress Address;
+        public List<Employment> EmploymentPositions { get; set; }
+
+        public Person()
+        {
+            //  if a instance of List<T> is not created and assigned then the property
+            //   will have an initial value of null
+            EmploymentPositions = new List<Employment>();
+        }
+
+    }
+}
