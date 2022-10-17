f8ac73db9d3bdeb869e76a4749db1eeeb6921511
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2022-10-17 10:31:03 -0600

    Razor Page Solution (Multi Days)

---
+++ src/CSRazorSolution/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string MyName { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Random random = new Random();
+            int value = random.Next(0, 100);
+            if (value % 2 == 0)
+            {
+                MyName = $"James welcome you to the Razor World ({value})";
+            }
+            else
+            {
+                MyName = null;
+            }
+        }
+    }
+}
