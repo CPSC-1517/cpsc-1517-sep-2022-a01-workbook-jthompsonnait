71779595c08dac3c67cc55778ac0c74c9ad7de65
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2022-10-18 13:46:45 -0600

    Multiple Buttons

--- src/CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ src/CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -13,7 +13,10 @@ namespace WebApp.Pages.Samples
         [BindProperty]
         public string Comments { get; set; }
 
-        [TempData]
+
+        //  Attribute TempData is requred IF you are processing multiple requests (OnPost
+        //      followed by OnGet() to retain data within the property
+        // [TempData]
         public string FeedBack { get; set; }
 
         public void OnGet()
@@ -24,7 +27,7 @@ namespace WebApp.Pages.Samples
             //      as the page is shown for the first time.
         }
 
-        public void OnPost()
+        public void OnPostA()
         {
             //  process the OnPost request of the form (method = "post")
             //  the return data type can be void or IActionResult
@@ -32,7 +35,20 @@ namespace WebApp.Pages.Samples
             //    specific process Post using the asp-page-handler
             //  logic the you wish to accomplish should be isolatted to the actions
             //    desired for the button.
-            FeedBack = $"Number {MyNum}, Course {FavouriteCourse} Comments {Comments}";
+            //  FeedBack = $"Number {MyNum}, Course {FavouriteCourse} Comments {Comments}";
+            FeedBack = "Button A was pressed";
+        }
+
+        public void OnPostB()
+        {
+            //  process the OnPost request of the form (method = "post")
+            //  the return data type can be void or IActionResult
+            //  OnPost request is the request from a <button> that has NOT indicated a 
+            //    specific process Post using the asp-page-handler
+            //  logic the you wish to accomplish should be isolatted to the actions
+            //    desired for the button.
+            //  FeedBack = $"Number {MyNum}, Course {FavouriteCourse} Comments {Comments}";
+            FeedBack = "Button B was pressed";
         }
     }
 }
